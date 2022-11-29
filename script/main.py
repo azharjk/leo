fa8dd5e747b84c1f35aebd75a791efc01b9498f7
azharjk <azhr.project@gmail.com>
2022-11-29 13:57:32 +0700

    add simple command line tool

--- script/main.py
+++ script/main.py
@@ -1,8 +1,14 @@
+import sys
 from core.fetcher import Fetcher
 
 
-def main():
-    r = Fetcher.Get("")
+def main() -> None:
+    if len(sys.argv) < 2:
+        print("ERROR: need to provide the url", file=sys.stderr)
+        sys.exit(1)
+
+    url = sys.argv[1]
+    r = Fetcher.Get(url)
     print(r.data)
 
 
